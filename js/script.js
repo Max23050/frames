fb33153319f1b9ec483907069d209ba606aef77a
Max <maxtaran708@gmail.com>
2024-02-29 23:40:10 +0100

    test-scroll

--- js/script.js
+++ js/script.js
@@ -15,9 +15,27 @@ document.addEventListener("DOMContentLoaded", function() {
         })
     })
 
+    document.querySelectorAll('.close-icon').forEach(button => {
+        button.addEventListener('click', function() {
+            const modal = this.closest('.modal');
+            closeModal(modal);
+        })
+    })
+
     document.querySelector('.modal-container').addEventListener('click', function(event) {
         if (event.target === this) {
             closeModal(document.querySelector('.modal:not([style*="display: none"])'));
+        }
+    })
+
+    window.addEventListener('scroll', function() {
+        const headerHeight = document.querySelector('.header').offsetHeight;
+        const scrollBlock = document.getElementById('scrollBlock');
+        if (window.pageYOffset > headerHeight) {
+            scrollBlock.style.top = '0';
+
+        } else {
+            scrollBlock.style.top = '-80px';
         }
     })
 })
@@ -32,11 +50,14 @@ function openModal(modalId) {
 
     document.querySelector(modalId).style.display = 'block';
     document.querySelector('.modal-container').style.display = 'flex';
+
+    document.body.style.overflow = 'hidden';
 }
 
 function closeModal(modal) {
     if (modal) {
         modal.style.display = 'none';
         document.querySelector('.modal-container').style.display = 'none';
+        document.body.style.overflow = '';
     }
 }
